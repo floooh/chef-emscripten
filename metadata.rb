1df536538d4c9fb2643842b9c9271fa1d3981463
Andre Weissflog <floooh@gmail.com>
2014-01-21 23:15:40 +0100

    Bumped version in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'floooh@gmail.com'
 license          'The MIT License (MIT)'
 description      'Install emscripten dev environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends          'apt'
 depends          'git'
 depends          'python'
